7a1dc088cdb18f945846f7bf8fdffe46d8e6ed57
Piotr Jelonek <piotrek.jelonek@icloud.com>
2025-05-10 17:20:40 +0200

    Created ApiClient.java

---
+++ src/main/java/org/pazurkota/API/ApiClient.java
@@ -0,0 +1,19 @@
+﻿package org.pazurkota.API;
+
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+
+public class ApiClient {
+    private final static HttpClient CLIENT = HttpClient.newHttpClient();
+    private final static String BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+    public static String getWeatherData(float lat, float lon) throws IOException, InterruptedException, NotImplementedException {
+        throw new NotImplementedException("This feature has been not implemented yet");
+    }
+
+    public static Integer[] getGeolocation(String cityName) throws IOException, InterruptedException, NotImplementedException {
+        throw new NotImplementedException("This feature has been not implemented yet");
+    }
+}
