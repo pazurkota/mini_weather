f962bc52c7b1ff7f8ff454d503c7457e77f34e24
Piotr Jelonek <piotrek.jelonek@icloud.com>
2025-05-10 17:36:52 +0200

    Implemented ApiClient.getGeolocation

--- src/main/java/org/pazurkota/API/ApiClient.java
+++ src/main/java/org/pazurkota/API/ApiClient.java
@@ -1,19 +1,38 @@
-﻿package org.pazurkota.API;
+package org.pazurkota.API;
 
+import com.google.gson.Gson;
 import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+import org.pazurkota.Model.Geolocation;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ApiClient {
     private final static HttpClient CLIENT = HttpClient.newHttpClient();
-    private final static String BASE_URL = "https://api.openweathermap.org/data/2.5";
+    private final static String BASE_URL = "https://api.openweathermap.org";
+    private final static String API_KEY = ApiKeyHandler.readKeyFromJsonFile();
 
     public static String getWeatherData(float lat, float lon) throws IOException, InterruptedException, NotImplementedException {
         throw new NotImplementedException("This feature has been not implemented yet");
     }
 
-    public static Integer[] getGeolocation(String cityName) throws IOException, InterruptedException, NotImplementedException {
-        throw new NotImplementedException("This feature has been not implemented yet");
+    public static Geolocation getGeolocation(String cityName) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + API_KEY))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            Geolocation[] geolocations = new Gson().fromJson(response.body(), Geolocation[].class);
+            if (geolocations.length > 0) {
+                return geolocations[0];
+            }
+        }
+        return null;
     }
 }
--- src/main/java/org/pazurkota/Main.java
+++ src/main/java/org/pazurkota/Main.java
@@ -1,9 +1,22 @@
 package org.pazurkota;
 
+import org.pazurkota.API.ApiClient;
+import org.pazurkota.Model.Geolocation;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        try {
+            Geolocation geolocation = ApiClient.getGeolocation("Wroclaw");
+
+            // check if geolocation is not null
+            assert geolocation != null;
+
+            System.out.println(geolocation.lat); // get latitude
+            System.out.println(geolocation.lon); // get longitude
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
