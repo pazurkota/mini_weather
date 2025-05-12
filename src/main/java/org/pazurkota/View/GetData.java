32c86fdf8b77de4c60b140d030eaaec119c12eee
Piotr Jelonek <piotrek.jelonek@icloud.com>
2025-05-12 18:49:30 +0200

    Created View/GetData

---
+++ src/main/java/org/pazurkota/View/GetData.java
@@ -0,0 +1,17 @@
+﻿package org.pazurkota.View;
+
+import org.pazurkota.API.DataParser;
+import org.pazurkota.Model.WeatherRoot;
+
+public class GetData {
+    private static float _lat;
+    private static float _lon;
+
+    private static final DataParser parser = DataParser.getInstance();
+    private static final WeatherRoot root = parser.parseWeatherData(_lat, _lon);
+
+    public GetData(float lat, float lon) {
+        _lat = lat;
+        _lon = lon;
+    }
+}
