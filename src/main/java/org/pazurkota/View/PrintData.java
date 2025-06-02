f81e1cee7072219134015d23d33c4b91838b8a4a
Piotr Jelonek <piotrek.jelonek@icloud.com>
2025-06-02 16:30:28 +0200

    Added wind data to PrintData

--- src/main/java/org/pazurkota/View/PrintData.java
+++ src/main/java/org/pazurkota/View/PrintData.java
@@ -16,6 +16,19 @@ public class PrintData {
         String data = "Current weather for " + weather.name + " is " + weather.weather.getFirst().description + ".\n" +
                 "Current temperature is " + weather.main.temp + "°C, but feels like " + weather.main.feels_like + "°C";
 
+        data += printWindData();
+
         System.out.println(data);
+    }
+
+    public String printWindData() {
+        float[] data = GetData.getWindData(weather);
+
+        String str = "\n\nCURRENT WIND DATA:\n";
+        str += "Current wind speed: " + data[0] + "\n";
+        str += "Current wind degree: " + data[1] + "\n";
+        str += "Current wind gusts: " + data[2] + "\n";
+
+        return str;
     }
 }
